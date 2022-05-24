a099de04f0725b81cf5e48c3423ced95594393aa
Steven Holtzen <sholtzen@gmail.com>
2022-05-24 16:53:30 -0400

    tests passing

--- src/manager/rsbdd_manager.rs
+++ src/manager/rsbdd_manager.rs
@@ -79,16 +79,16 @@ pub struct BddWmc<T: Num + Clone + Debug + Copy> {
     pub one: T,
     /// a vector which maps variable labels to `(low, high)`
     /// valuations.
-    var_to_val: Vec<(T, T)>,
+    var_to_val: Vec<Option<(T, T)>>,
 }
 
 impl<T: Num + Clone + Debug + Copy> BddWmc<T> {
     /// Generates a new `BddWmc` with a default `var_to_val`; it is private because we
     /// do not want to expose the structure of the associative array
     pub fn new_with_default(zero: T, one: T, var_to_val: HashMap<VarLabel, (T, T)>) -> BddWmc<T> {
-        let mut var_to_val_vec = vec![(zero, zero); var_to_val.len()];
+        let mut var_to_val_vec : Vec<Option<(T,T)>> = vec![None; var_to_val.len()];
         for (key, value) in var_to_val.iter() {
-            var_to_val_vec[key.value_usize()] = *value;
+            var_to_val_vec[key.value_usize()] = Some(*value);
         }
         BddWmc {
             zero: zero,
@@ -111,20 +111,24 @@ impl<T: Num + Clone + Debug + Copy> BddWmc<T> {
         let mut prod = self.one;
         for lit in assgn.iter() {
             if lit.get_polarity() {
-                prod = prod * self.var_to_val[lit.get_label().value_usize()].1
+                prod = prod * self.var_to_val[lit.get_label().value_usize()].unwrap().1
             } else {
-                prod = prod * self.var_to_val[lit.get_label().value_usize()].0
+                prod = prod * self.var_to_val[lit.get_label().value_usize()].unwrap().0
             }
         }
         return prod;
     }
 
-    pub fn set_weight(&self, lbl: VarLabel, low: T, high: T) -> () {
-        todo!()
+    pub fn set_weight(&mut self, lbl: VarLabel, low: T, high: T) -> () {
+        let n = lbl.value_usize();
+        while n >= self.var_to_val.len() {
+            self.var_to_val.push(None);
+        }
+        self.var_to_val[n] = Some((low, high));
     }
 
     pub fn get_var_weight(&self, label: VarLabel) -> &(T, T) {
-        return &self.var_to_val[label.value_usize()];
+        return &(self.var_to_val[label.value_usize()]).as_ref().unwrap();
     }
 }
 
