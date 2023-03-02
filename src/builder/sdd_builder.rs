c1eef1ba48c0395cd5ae797dac9edddf3653aec1
Matthew Wang <matt@matthewwang.me>
2023-03-02 12:21:16 -0800

    Run `cargo fmt`

--- src/builder/sdd_builder.rs
+++ src/builder/sdd_builder.rs
@@ -805,7 +805,7 @@ impl SddManager {
     }
 
     /// get an iterator over all unique allocated nodes by the manager
-    pub fn node_iter(&self) -> impl Iterator<Item = SddPtr> + '_ { 
+    pub fn node_iter(&self) -> impl Iterator<Item = SddPtr> + '_ {
         let bdditer = self.bdd_iter().map(|x| SddPtr::bdd(x));
         self.or_iter().map(|x| SddPtr::Reg(x)).chain(bdditer)
     }
