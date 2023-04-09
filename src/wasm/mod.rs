ccc7162e5204c070d37b0f61c17438972e06e6ef
Matt Wang <matt@matthewwang.me>
2023-04-09 00:40:35 -0700

    Experimental: WASM support (#71)
    This PR is the minimum necessary work required to get `rsdd` to work with `wasm-pack`.
    
    In order, I:
    
    1. `cargo install wasm-pack`
    2. add `wasm-bindgen` to `Cargo.toml`
    3. observe that any WASM build fails because of deps:
        - need to opt-in to JS features for `getrandom`
        - ~~need to bump `num` (and its dependents) to stop using `rustc_serializer`~~ did this in #73 
     4. add some code that uses `#[wasm_bindgen]`; I did two simple functions in `wasm/mod.rs`
     5. build with `wasm-pack build`
    
    I used the resulting WASM binary to create [indecision](https://inddecision.netlify.app/), which is a quick frontend tech demo that runs rsdd in your browser. Give it a spin!
    
    Notably, we didn't need to otherwise change any of the source code :)
    
    ---
    
    The restriction that you can't use generic functions seems to still be there? I've gotten around this by making both functions `String -> String`. I think a way forward involves making more concrete types for things like `SddManager`, etc.

---
+++ src/wasm/mod.rs
@@ -0,0 +1,60 @@
+use crate::builder::bdd_builder::{BddManager, BddPtr};
+use crate::builder::cache::lru_app::BddApplyTable;
+use crate::builder::canonicalize::CompressionCanonicalizer;
+use crate::builder::sdd_builder::SddManager;
+use crate::repr::{cnf::Cnf, var_label::VarLabel, vtree::VTree};
+use crate::serialize::{ser_bdd, ser_sdd, ser_vtree};
+use wasm_bindgen::prelude::*;
+
+// used in: https://github.com/mattxwang/indecision
+#[wasm_bindgen]
+pub fn get_vtree(cnf_input: String) -> String {
+    let cnf = Cnf::from_file(cnf_input);
+
+    let range: Vec<usize> = (0..cnf.num_vars() + 1).collect();
+    let binding = range
+        .iter()
+        .map(|i| VarLabel::new(*i as u64))
+        .collect::<Vec<VarLabel>>();
+    let vars = binding.as_slice();
+
+    let vtree = VTree::right_linear(vars);
+
+    let json = ser_vtree::VTreeSerializer::from_vtree(&vtree);
+    serde_json::to_string(&json).unwrap()
+}
+
+// used in: https://github.com/mattxwang/indecision
+#[wasm_bindgen]
+pub fn get_bdd(cnf_input: String) -> String {
+    let cnf = Cnf::from_file(cnf_input);
+
+    let mut man = BddManager::<BddApplyTable<BddPtr>>::new_default_order_lru(cnf.num_vars());
+    let bdd = man.from_cnf(&cnf);
+
+    let json = ser_bdd::BDDSerializer::from_bdd(bdd);
+
+    serde_json::to_string(&json).unwrap()
+}
+
+// used in: https://github.com/mattxwang/indecision
+#[wasm_bindgen]
+pub fn get_sdd(cnf_input: String) -> String {
+    let cnf = Cnf::from_file(cnf_input);
+
+    let range: Vec<usize> = (0..cnf.num_vars() + 1).collect();
+    let binding = range
+        .iter()
+        .map(|i| VarLabel::new(*i as u64))
+        .collect::<Vec<VarLabel>>();
+    let vars = binding.as_slice();
+
+    let vtree = VTree::right_linear(vars);
+
+    let mut compr_mgr = SddManager::<CompressionCanonicalizer>::new(vtree);
+    let sdd = compr_mgr.from_cnf(&cnf);
+
+    let json = ser_sdd::SDDSerializer::from_sdd(sdd);
+
+    serde_json::to_string(&json).unwrap()
+}
