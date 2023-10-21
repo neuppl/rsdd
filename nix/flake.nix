7733e5e33f199da472839b89b3e2531a5d46b310
Sam Stites <stites@users.noreply.github.com>
2023-10-21 01:32:00 -0400

    bump flake

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,108 +2,74 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     devenv.url = "github:cachix/devenv";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
-
-  outputs = {
-    self,
-    nixpkgs,
-    devenv,
-    ...
-  } @ inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-        ];
-      };
-      naersk = pkgs.callPackage inputs.naersk {};
-      rsdd = pkgs.callPackage ./. {};
-      py = pkgs.python3.withPackages (p: [p.graphviz]);
-      render-graphviz = pkgs.writeScriptBin "render-graphviz" ''
-        ${py}/bin/python ${../bin/render_graphviz.py}
-      '';
-    in {
-      packages = {
-        inherit rsdd render-graphviz;
+  outputs = inputs@{ ... }: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    imports = [ inputs.devenv.flakeModule ];
+    systems = [ "x86_64-linux" ];
+    perSystem = { config, pkgs, ... }: {
+      packages = rec {
         default = rsdd;
+        rsdd = pkgs.callPackage ./. {};
+        render-graphviz = let
+          py = pkgs.python3.withPackages (p: [p.graphviz]);
+        in pkgs.writeScriptBin "render-graphviz" ''
+          ${py}/bin/python ${../scripts/render_graphviz.py}
+        '';
       };
-      apps = rec {
+      apps = {
         render-graphviz = {
           type = "app";
-          program = "${render-graphviz}/bin/render-graphviz";
+          program = "${config.packages.render-graphviz}/scripts/render-graphviz";
         };
-        bayesian_network_compiler = {
+        bottomup_cnf_to_bdd = {
           type = "app";
-          program = "${rsdd}/bin/bayesian_network_compiler";
+          program = "${config.packages.rsdd}/bin/bottomup_cnf_to_bdd";
         };
-        compare_canonicalize = {
+        bottomup_formula_to_bdd = {
           type = "app";
-          program = "${rsdd}/bin/compare_canonicalize";
+          program = "${config.packages.rsdd}/bin/bottomup_formula_to_bdd";
         };
-        one_shot_benchmark = {
+        weighted_model_count = {
           type = "app";
-          program = "${rsdd}/bin/one_shot_benchmark";
+          program = "${config.packages.rsdd}/bin/weighted_model_count";
         };
       };
-
-      devShell = inputs.devenv.lib.mkShell {
-        inherit inputs pkgs;
-        modules = [
-          rec {
-            pre-commit.hooks = {
-              shellcheck.enable = true;
-              clippy.enable = true;
-              hunspell.enable = true;
-              alejandra.enable = true;
-              rustfmt.enable = true;
-              typos.enable = true;
-            };
-            languages.rust.enable = true;
-            # add a rust-repl
-            scripts.repl.exec = "${pkgs.evcxr}/bin/evcxr";
-            env.DEVSHELL = "devshell+flake.nix";
-            enterShell = pkgs.lib.strings.concatStringsSep "\n" ([
-                ''
-                  echo ""
-                  echo "Hello from $DEVSHELL!"
-                  echo "Some tools this environment is equipped with:"
-                  echo ""
-                ''
-              ]
-              ++ (builtins.map (
-                  p: "echo \"${p.pname}\t\t-- ${p.meta.description}\""
-                )
-                packages)
-              ++ ["echo \"\""]);
-
-            packages =
-              with pkgs;
-                [
-                  lldb
-                  cargo
-                  rustc
-                  rustfmt
-                  rust-analyzer
-                  clippy
-                  cargo-watch
-                  cargo-nextest
-                  cargo-expand # expand macros and inspect the output
-                  cargo-llvm-lines # count number of lines of LLVM IR of a generic function
-                  cargo-inspect
-                  cargo-criterion
-                  evcxr # make sure repl is in a gc-root
-                  cargo-play # quickly run a rust file that has a maint function
-                ]
-                ++ lib.optionals stdenv.isDarwin []
-                ++ lib.optionals stdenv.isLinux [
-                  cargo-rr
-                  rr-unstable
-                ]
-              # ++ builtins.attrValues self.checks
-              ;
-          }
-        ];
+      devenv.shells.default = {
+        pre-commit.hooks = {
+          shellcheck.enable = true;
+          clippy.enable = true;
+          hunspell.enable = true;
+          alejandra.enable = true;
+          rustfmt.enable = true;
+          typos.enable = true;
+        };
+        languages.rust.enable = true;
+        #languages.rust.version = "stable";
+        scripts.repl.exec = "${pkgs.evcxr}/bin/evcxr";
+        packages = with pkgs; [
+          lldb
+          cargo
+          rustc
+          rustfmt
+          rust-analyzer
+          clippy
+          cargo-watch
+          cargo-nextest
+          cargo-expand # expand macros and inspect the output
+          cargo-llvm-lines # count number of lines of LLVM IR of a generic function
+          cargo-inspect
+          cargo-criterion
+          evcxr # make sure repl is in a gc-root
+          cargo-play # quickly run a rust file that has a maint function
+        ]
+        ++ lib.optionals stdenv.isDarwin []
+        ++ lib.optionals stdenv.isLinux [
+          cargo-rr
+          rr-unstable
+        ]
+        ;
       };
-    });
+    };
+  };
 }
