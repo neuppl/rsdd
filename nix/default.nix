39aadfb9a7394e25396144a52616bfdabf78ed74
Sam Stites <stites@users.noreply.github.com>
2023-10-24 10:54:09 -0400

    bump nix flake (#183)

--- nix/default.nix
+++ nix/default.nix
@@ -5,17 +5,22 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "rsdd";
-  version = "unstable-2023-03-03";
+  version = "0.1.0";
 
   src = ../.;
 
-  cargoHash = "sha256-mi27RanxrNy6ncpFUAvR62gSj+JnvLc1BPZD4NI56yg=";
+  #cargoHash = "sha256-GYO554P3VHilJm2z/BM8noiPKG/HQM5jSELwuXAHJAA=";
+  cargoHash = "sha256-dl1R1VHPPwa2RVZw3as7zbmKUSrWvjTrMYn/YEYw/38=";
+
   cargoPatches = [./0001-Cargo.lock.patch];
+
+
+  buildFeatures = [ "ffi" "cli" ];
 
   meta = with lib; {
     description = "Rust decision diagrams";
     homepage = "https://github.com/neuppl/rsdd";
     license = licenses.mit;
-    maintainers = with maintainers; [];
+    maintainers = with maintainers; [ stites ];
   };
 }
