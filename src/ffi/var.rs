c3c6b5cd192baa05a327d36ac47f75bedc5ff534
Brianna Marshall <brianna@bmarsha.com>
2024-05-11 12:26:43 -0400

    VarOrder constructor

--- src/ffi/var.rs
+++ src/ffi/var.rs
@@ -1,11 +1,13 @@
-use crate::repr::{Literal, VarLabel};
-
-#[no_mangle]
-pub extern "C" fn var_label_new(label: u64) -> VarLabel {
-    VarLabel::new(label)
-}
+use crate::repr::{Literal, VarLabel, VarOrder};
+use core::slice;
 
 #[no_mangle]
 pub extern "C" fn literal_new(label: VarLabel, polarity: bool) -> Literal {
     Literal::new(label, polarity)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn var_order_new(order: *const VarLabel, len: usize) -> *mut VarOrder {
+    let order = slice::from_raw_parts(order, len);
+    Box::into_raw(Box::new(VarOrder::new(order)))
 }
